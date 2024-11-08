7bfc759780825dfda60b8c2cd06943247afd528c
ugr0 <yujiro.takahashi0912@gmail.com>
2024-11-08 09:40:23 +0900

    feat: suspendを検証

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Suspense } from 'react'
 import './App.css'
+import { AlwaysSuspend } from './components/AlwaysSuspend'
 
 function App() {
   return (
     <div className="text-center">
       <h1 className="text-2xl">React App!</h1>
+      <Suspense fallback={<p>Loading...</p>}>
+        <AlwaysSuspend />
+      </Suspense>
     </div>
   )
 }
